12cb6b440571007c047ee1ccfcb3b66b612ea9ac
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-14 08:34:41 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,69 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import './../styles/App.css';
+import Todo from './Todo';
+const App = () => {
+    const [dish, setDish] = useState(Data);
+    const filterDish = (category) => {
+      if (category === "all") {
+        setDish(Data); // Reset to display all dishes
+      } else {
+        const newData = Data.filter((item) => item.category === category);
+        setDish(newData);
+      }
+    };
+  return (
+    <>
+      <h1>Our Menu</h1>
+      <div className="btn-group">
+        <button
+          onClick={() => {
+            filterDish("all");
+          }}
+        >
+          All
+        </button>
+        <button
+          onClick={() => {
+            filterDish("breakfast");
+          }}
+        >
+          Breakfast
+        </button>
+        <button
+          onClick={() => {
+            filterDish("lunch");
+          }}
+        >
+          Lunch
+        </button>
+        <button
+          onClick={() => {
+            filterDish("shakes");
+          }}
+        >
+          Shakes
+        </button>
+      </div>
+
+      <div className="product-div">
+        {dish.map((items) => {
+          return (
+            <>
+              <img src={items.img} width={200} height={200} />
+              <div className="align-vertical">
+                <div className="content">
+                  <h3>{items.title}</h3>
+                  <p className="price">{items.price}</p>
+                </div>
+                <p className="description">{items.desc}</p>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
 
